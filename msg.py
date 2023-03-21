c2c9b10e4d57fb3de29f907cfb8d585fdb5b1413
Ansh Modi <74901469+anshmodi27@users.noreply.github.com>
2023-03-21 10:31:43 +0530

    Update msg.py

--- msg.py
+++ msg.py
@@ -4,7 +4,7 @@ import time
 try:
     while True:
         time.sleep(0.5)
-        pyautogui.typewrite('Good News\nBokhra Party Is BAck')
+        pyautogui.typewrite('Hello')
         time.sleep(0.5)
         pyautogui.press("enter")
 except KeyboardInterrupt:
